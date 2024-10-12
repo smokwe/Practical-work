0abbdf8a97b82e3700c97c2dd7b8c9c0df3d901f
smk <157803130+smokwe@users.noreply.github.com>
2024-10-12 15:45:51 +0500

    Add files via upload

---
+++ Practical work №6 WPF/практическая работа №6 WPF/практическая работа №6 WPF/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практическая_работа__6_WPF
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
